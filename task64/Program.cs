c7413461b4f65ba29b100377c77cf8c1ef1d4cd8
VladislavLipkinGit <lipkin1993vladislav@gmail.com>
2023-09-02 21:52:40 +0300

    task64 ready

--- task64/Program.cs
+++ task64/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите число: ");
+int EnNum = int.Parse(Console.ReadLine()!);
+Console.WriteLine(PrintNum(EnNum, 1));
+
+string PrintNum(int EndNumber, int StartNumber){
+    if (EndNumber == StartNumber) return StartNumber.ToString();
+    return (EndNumber + ", " + PrintNum(EndNumber-1, StartNumber));
+}
